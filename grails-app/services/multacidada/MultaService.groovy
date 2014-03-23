772f89d47b94b305649fa7beb2f38bdbfffc7eb4
Roberto Speicys Cardoso <roberto.speicys_cardoso@ambientic.com>
2014-03-23 11:55:29 -0300

    Novas fotos

--- grails-app/services/multacidada/MultaService.groovy
+++ grails-app/services/multacidada/MultaService.groovy
@@ -11,13 +11,20 @@ class MultaService {
 
 	def load() {
 
-		new Multa(userID:"111", latitude:-23, longitude:-46,fotoURL:"/fotos/cet3.jpg",tipo:1, data:"2014-03-12 12:40").save(failOnError: true);
-		new Multa(userID:"222", latitude:-23.2, longitude:-46.2,fotoURL:"/fotos/cet2.jpg", tipo:2, data:"2014-03-14 13:20").save(failOnError: true);
 		new Multa(userID:"111", latitude:-23, longitude:-46,fotoURL:"/fotos/cet3.jpg",tipo:1, data:"2014-03-15 10:00").save(failOnError: true);
 		new Multa(userID:"222", latitude:-23.2, longitude:-46.2,fotoURL:"/fotos/cet2.jpg", tipo:2, data:"2014-03-15 15:00").save(failOnError: true);
-		//new Teste(title:"The Stand").save()
-		//new Teste(title:"The Shining").save()
-
+		new Multa(userID:"222", latitude:-23.2, longitude:-46.2,fotoURL:"/fotos/fina-educativa.jpg", tipo:8, data:"2014-03-15 15:00").save(failOnError: true);
+		new Multa(userID:"222", latitude:-23.2, longitude:-46.2,fotoURL:"/fotos/ciclovia-bebado.jpg", tipo:8, data:"2014-03-15 15:00").save(failOnError: true);
+		new Multa(userID:"222", latitude:-23.2, longitude:-46.2,fotoURL:"/fotos/farolvermelho.jpg", tipo:0, data:"2014-03-15 15:00").save(failOnError: true);
+		new Multa(userID:"222", latitude:-23.2, longitude:-46.2,fotoURL:"/fotos/busao-farol-vermelho.jpg", tipo:0, data:"2014-03-15 15:00").save(failOnError: true);
+		new Multa(userID:"222", latitude:-23.2, longitude:-46.2,fotoURL:"/fotos/ferrari-deficiente.jpg", tipo:4, data:"2014-03-15 15:00").save(failOnError: true);
+		new Multa(userID:"222", latitude:-23.2, longitude:-46.2,fotoURL:"/fotos/duplo-vermelho.jpg", tipo:0, data:"2014-03-15 15:00").save(failOnError: true);
+		new Multa(userID:"222", latitude:-23.2, longitude:-46.2,fotoURL:"/fotos/paulista.jpg", tipo:3, data:"2014-03-15 15:00").save(failOnError: true);
+		new Multa(userID:"222", latitude:-23.2, longitude:-46.2,fotoURL:"/fotos/fusca.jpg", tipo:7, data:"2014-03-15 15:00").save(failOnError: true);
+		new Multa(userID:"222", latitude:-23.2, longitude:-46.2,fotoURL:"/fotos/kombi.png", tipo:7, data:"2014-03-15 15:00").save(failOnError: true);
+		
+		
+		
 		println Multa.list()
 	}
 
