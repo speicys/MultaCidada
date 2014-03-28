3185ee0dfbf623492c566b3158e13e38e95db077
Julian Monteiro <julian.monteiro@gmail.com>
2014-03-27 23:25:22 -0300

    mysql config

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -20,15 +20,17 @@ environments {
 			url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
 		}
 	}
+	
 	test {
 		dataSource {
 			dbCreate = "update"
 			url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
 		}
 	}
+	
 	production {
 		dataSource {
-			dbCreate = "update"
+			dbCreate = "create"
 			url = "jdbc:mysql://127.0.0.1:3306/marronzinho"
 			driverClassName = "com.mysql.jdbc.Driver"
 			username = "devuser"
